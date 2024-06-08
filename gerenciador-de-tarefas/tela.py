63618fefca38c4bbe2eabf9facc867bbae1092e4
Merfik <mersfik@gmail.com>
2024-06-08 14:48:52 -0300

    new add

--- gerenciador-de-tarefas/tela.py
+++ gerenciador-de-tarefas/tela.py
@@ -7,9 +7,7 @@ tela.title("Projeto")
 tela.config(background="grey")
 tela.iconbitmap("favicon.ico")
 
-#creating a new botton with tkinter:
-botao_1 = CTkButton(tela, width=200, height=30, text="Clique aqui!", bg_color="grey")
-botao_1.pack(pady= 10)
+
 
 
 tela.mainloop()
