fb2ab701ae96216884a234e5536983b69901366a
Merfik <mersfik@gmail.com>
2024-06-10 18:50:02 -0300

    new line

--- texts.py
+++ texts.py
@@ -1,1 +1,6 @@
-from selenium import webdriver
+import socket
+client = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("bancocn.com", 80))
+client.send(b"Hello World")
+resposta = client.recv(1024)
+print (resposta)
