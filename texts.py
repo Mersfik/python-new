5a65fd5175ea0b1cd96d2f07e27548acb61a574c
Merfik <mersfik@gmail.com>
2024-06-11 14:28:58 -0300

    new line

--- texts.py
+++ texts.py
@@ -1,6 +1,17 @@
 import socket
-client = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("bancocn.com", 80))
-client.send(b"Hello World")
-resposta = client.recv(1024)
-print (resposta.decode())
+
+ports = [21,22,80,443,3306,25]
+
+for port in ports:
+
+    client = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+    client.settimeout(2.5)
+    code = client.connect_ex(("bancocn.com", port))
+
+    if code == 0:
+        client.send(b"Hello World")
+        resposta = client.recv(1024)
+        print(resposta.decode())
+
+    
+    
